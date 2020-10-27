031b55621d0028c7b86a61023b368c75a22280ce
olasuchi <ilesanmiolaide001@gmail.com>
2020-10-27 03:01:59 +0100

    building site content

--- JS/main.js
+++ JS/main.js
@@ -10,11 +10,16 @@ $(document).ready(function(){
         
      })
 
-     /**owl carousel */
+     /**owl carousel navtext for settin arrow(setting the blog post to slideshow automaticaly every 3sec and to design the navigation pane)*/
 
-     $('.owl-carousel').owlCarousel();
-
-
+     $('.owl-carousel').owlCarousel({
+       loop:true,
+       autoplay:true,
+       autoplayTimeout:3000,
+       dots:false,
+       nav:true,
+       navText:[$('.owl-navigation .nav-prev'),$('.owl-navigation .nav-next')]
+     });
 
 
 });
