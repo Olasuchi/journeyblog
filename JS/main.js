755f0e43098f03ab545f0adca1c0cb3f137728d0
olasuchi <ilesanmiolaide001@gmail.com>
2020-12-01 00:23:14 +0100

    final comit. ready

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,18 @@
+const responsive = {
+  0  : {
+    items:1
+  },
+  320 : {
+    items:1
+  },
+  560: {
+    items:2
+  },
+  960: {
+    items:3
+  }
+}
+
 $(document).ready(function(){
 
     $nav=$('.nav');
@@ -18,8 +33,18 @@ $(document).ready(function(){
        autoplayTimeout:3000,
        dots:false,
        nav:true,
-       navText:[$('.owl-navigation .nav-prev'),$('.owl-navigation .nav-next')]
+       navText:[$('.owl-navigation .nav-prev'),$('.owl-navigation .nav-next')],
+       responsive: responsive
      });
 
 
+     //click to scroll to the top move up arrow bottom right corner
+     $('.move-up span').click(function(){
+       $('html,body').animate({
+         scrollTop:0
+       },1000);
+     })
+
+// AOS instance 
+AOS.init();
 });
